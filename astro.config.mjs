fa5d21f99d4ba66d14d44b964821e5385c6d601e
emrepbu <33737009+emrepbu@users.noreply.github.com>
2025-02-05 23:13:06 +0300

    Add LaTeX

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,8 @@
 import { defineConfig } from "astro/config";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
+import remarkMath from 'remark-math';
+import rehypeKatex from 'rehype-katex';
 
 // https://astro.build/config
 export default defineConfig({
@@ -16,5 +18,7 @@ export default defineConfig({
       },
       wrap: true,
     },
+    remarkPlugins: [remarkMath],
+    rehypePlugins: [rehypeKatex],
   },
 });
